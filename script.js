faa68aa370715a77bc98574a7addfb7770854c0e
Jose Pablo <30580990+josepablomc@users.noreply.github.com>
2021-06-03 23:08:01 -0600

    AddingJSExternalFile
    I added an external File for JS only, then called it using the <script> tag in the html head.

---
+++ script.js
@@ -0,0 +1,28 @@
+
+    function myFunction() {
+        var cedula = document.getElementById("txtCedula").value;
+        var haciendaAPI = "https://api.hacienda.go.cr/fe/ae?identificacion=" + cedula;
+
+        //API Hacienda
+        var request = new XMLHttpRequest();
+        request.open('GET', haciendaAPI);
+        request.onload = function () {
+            var jsonResponse = JSON.parse(request.responseText);
+            console.log(jsonResponse);
+
+            var nombre = jsonResponse.nombre;
+            document.getElementById("Nombre").value = nombre;
+
+            var moroso = jsonResponse.situacion.moroso;
+            document.getElementById("Moroso").value = moroso;
+                        
+            if (jsonResponse.actividades.length == 0) {
+                document.getElementById("Descripcion").value = "SIN DESCRIPCION";
+            }
+            else {
+                var descripcion = jsonResponse.actividades[0].descripcion;
+                document.getElementById("Descripcion").value = descripcion;
+            }
+        };
+        request.send();
+    }
