a3d64e480ba4d3ef55cbe551b41af840ff94e549
Jose Pablo <30580990+josepablomc@users.noreply.github.com>
2021-06-05 19:02:41 -0600

    Added a for loop to the JS to iterate over the user activities list, also changed text input to textarea and changed the size of the .box class

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 
-    function myFunction() {
-        var cedula = document.getElementById("txtCedula").value;
-        var haciendaAPI = "https://api.hacienda.go.cr/fe/ae?identificacion=" + cedula;
+function myFunction() {
+    var cedula = document.getElementById("txtCedula").value;
+    var haciendaAPI = "https://api.hacienda.go.cr/fe/ae?identificacion=" + cedula;
 
-        //API Hacienda
-        var request = new XMLHttpRequest();
-        request.open('GET', haciendaAPI);
-        request.onload = function () {
-            var jsonResponse = JSON.parse(request.responseText);
-            console.log(jsonResponse);
+    //API Hacienda
+    var request = new XMLHttpRequest();
+    request.open('GET', haciendaAPI);
+    request.onload = function () {
+        var jsonResponse = JSON.parse(request.responseText);
+        console.log(jsonResponse);
 
-            var nombre = jsonResponse.nombre;
-            document.getElementById("Nombre").value = nombre;
+        var nombre = jsonResponse.nombre;
+        document.getElementById("Nombre").value = nombre;
 
-            var moroso = jsonResponse.situacion.moroso;
-            document.getElementById("Moroso").value = moroso;
-                        
-            if (jsonResponse.actividades.length == 0) {
-                document.getElementById("Descripcion").value = "SIN DESCRIPCION";
+        var moroso = jsonResponse.situacion.moroso;
+        document.getElementById("Moroso").value = moroso;
+
+        if (jsonResponse.actividades.length == 0) {
+            document.getElementById("Descripcion").value = "SIN DESCRIPCION";
+        }
+        else {
+            var x;
+            var descripcion = jsonResponse.actividades[0].descripcion;
+            for (x = 1; x < jsonResponse.actividades.length; x++) {
+                descripcion = descripcion+", \n"+jsonResponse.actividades[x].descripcion;
             }
-            else {
-                var descripcion = jsonResponse.actividades[0].descripcion;
-                document.getElementById("Descripcion").value = descripcion;
-            }
-        };
-        request.send();
-    }
+            document.getElementById("Descripcion").value = descripcion;
+        }
+    };
+    request.send();
+}
